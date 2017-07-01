6705a69da32020910fc1df57b52c7c5c6f2bd6df
Terrence Wong <wongterrencew@gmail.com>
2017-07-01 18:51:00 +0200

    style h1

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 import React, { Component } from 'react'
+import styled from 'styled-components'
+
+const H1 = styled.h1`
+  font-family: 'Comic Sans MS';
+`
 
 export default class App extends Component {
   render () {
-    return <h1>Hello, World!</h1>
+    return <H1>Hello, World!</H1>
   }
 }
