b2a586d91743360881cb4c094cd100a081ce559a
Terrence Wong <wongterrencew@gmail.com>
2017-07-01 18:35:38 +0200

    make it stateful

--- app.js
+++ app.js
@@ -1,7 +1,20 @@
 import React, { Component } from 'react'
 
 export default class App extends Component {
+  input: null
+
+  state = {
+    text: ''
+  }
+
+  handleChange = () => {
+    this.setState({ text: this.input.value })
+  }
+
   render () {
-    return <h1>Hello, World!</h1>
+    return <div>
+      <h1>{this.state.text}</h1>
+      <input type='text' ref={ref => this.input = ref} onChange={this.handleChange} />
+    </div>
   }
 }
